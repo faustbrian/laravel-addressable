47b1495cf19b74d387dcf9d4efac9d9d4edb0e89
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-15 08:00:52 +0200

    Remove CRUD methods from interface

--- src/Interfaces/HasAddresses.php
+++ src/Interfaces/HasAddresses.php
@@ -31,28 +31,6 @@ interface HasAddresses
     public function shippingAddress($address);
 
     /**
-     * @param $data
-     *
-     * @return mixed
-     */
-    public function createAddress($data);
-
-    /**
-     * @param $address
-     * @param $data
-     *
-     * @return mixed
-     */
-    public function updateAddress($address, $data);
-
-    /**
-     * @param $address
-     *
-     * @return mixed
-     */
-    public function deleteAddress($address);
-
-    /**
      * @param $distance
      * @param $type
      * @param $lat
