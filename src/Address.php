b65b13c8aa0a9006788098ede763195790d5f055
Brian Faust <faustbrian@users.noreply.github.com>
2017-01-20 16:16:06 +0200

    add missing import

--- src/Address.php
+++ src/Address.php
@@ -22,6 +22,7 @@ declare(strict_types=1);
 
 namespace BrianFaust\Addressable;
 
+use BrianFaust\Countries\Models\Country;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
